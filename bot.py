65676a81651b98f763a0cf0b73ed0ba936d3282f
Joe Pringle <explodingp@gmail.com>
2014-05-11 15:31:24 -0400

    add simple bot

---
+++ bot.py
@@ -0,0 +1,39 @@
+from connect import connect
+
+class Message:
+    """Represent IRC message"""
+
+    def __init__(self, buf):
+        if ("PING" not in buf and "PRIVMSG" not in buf):
+            self.sender = self.contents = None
+        else:
+            self.sender   = buf.split('!')[0].strip(':')
+            self.contents = buf.split(':')[-1].strip()
+
+    def is_ping(self):
+        return self.sender == "PING"
+
+if __name__ == "__main__":
+    s = connect()
+
+    while(True):
+        # Call recv()
+        readbuffer = s.recv(1024).decode("UTF-8", errors = "ignore")
+        for msg in readbuffer.split('\n'):
+            msg = Message(msg)
+
+            # Ignore blank messages
+            if (msg.sender is None):
+                continue
+
+            print("SENDER: {} CONTENTS: {}\n".format(
+                msg.sender, msg.contents))
+
+            if (msg.is_ping()):
+                print("Received a ping! Pong-ing...")
+                s.send(bytes("PONG tmi.twitch.tv\r\n", "UTF-8"))
+    
+
+
+
+
